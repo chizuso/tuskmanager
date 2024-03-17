d05adbaeb78092586c45cfc75fe493e671c08c38
chizuso <80586453+chizuso@users.noreply.github.com>
2024-03-17 14:42:22 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -136,6 +136,17 @@ def check(todo_id):
     flash('Todo-Status aktualisiert.')
     return redirect(url_for('index'))
 
+# Route für API
+@app.route('/api/getToDo/<userid>')
+def api_getToDo(userid):
+    todo = ToDo.query.filter_by(user_id = userid)
+    return jsonify([s.toDict() for s in todo])
+ 
+@app.route('/api/getallusers')
+def api_getallusers():
+    user = User.query.all()
+    return jsonify([s.toDict() for s in user])
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
