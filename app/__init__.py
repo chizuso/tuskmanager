c92245b684fd60012906bb4ba2d19c8668e58a53
chizuso <80586453+chizuso@users.noreply.github.com>
2024-03-17 15:03:16 +0100

    Update __init__.py
    api fehlerbehebung

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonfiy
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
